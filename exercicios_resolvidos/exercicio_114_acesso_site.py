7642a70483ec65b4083e707421313af9adb006b1
Francieli Muniz <muniz_francy_paramore@hotmail.com>
2025-08-31 18:28:16 -0300

    Exercício 114

---
+++ exercicios_resolvidos/exercicio_114_acesso_site.py
@@ -0,0 +1,15 @@
+'''
+Crie um código em Python que teste se o site Pudim está acessível
+pelo computador utilizado.
+'''
+
+import urllib
+import urllib.request
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site Pudim não está acessível no momento.')
+else:
+    print('Consegui acessar o site Pudim :)')
+    #print(site.read())
